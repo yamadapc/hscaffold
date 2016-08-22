7c612e9f7bc6257448417f01b92b6354ea1d52af
yamadapc <tacla.yamada@gmail.com>
2016-08-22 16:56:13 -0300

    Fix haddock bug

--- src/Hscaffold/Interpreter/Common.hs
+++ src/Hscaffold/Interpreter/Common.hs
@@ -4,11 +4,10 @@ import           System.FilePath
 
 -- | Very simple helper, if the second argument is absolute, returns it,
 -- otherwise, return it relative to the first argument
-mkActionPath :: FilePath
-             ->
-             -- ^ The root
-             FilePath
-             ->
-             -- ^ A filepath
-             FilePath
+mkActionPath
+    :: FilePath
+    -- ^ The root
+    -> FilePath
+    -- ^ A filepath
+    -> FilePath
 mkActionPath root fp = if isAbsolute fp then fp else root </> fp
