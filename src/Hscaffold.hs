20d2dc9bc9e7179f7d870e296018c4a2764dcce2
yamadapc <tacla.yamada@gmail.com>
2016-08-22 14:50:36 -0300

    Workspace update

--- src/Hscaffold.hs
+++ src/Hscaffold.hs
@@ -1,15 +1,28 @@
 -- module: Hscaffold
 -- author: Pedro Tacla Yamada
 -- synopsis: Very simple file/directory structure scaffolding writer monad EDSL
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 module Hscaffold
     (
     -- * Running Hscaffold
       runHscaffold
+    -- * Convert Hscaffold to HSFILES (@stack templates@)
+    , toHsfiles
+    , writeToHsfiles
+    -- * Convert HSFILES to Hscaffold
+    , fromHsfiles
+    , fromHsfilesW
+    , readHsfiles
+
+    -- * Convert a directory to Hscaffold
+    , hscaffoldFromDirectory
+    -- * Compile Hscaffold to Haskell code
+    , hscaffoldToHaskell
+
+    -- ** Finer grained runners
     , runAction
     , runWriter
     , runWriterT
@@ -30,12 +43,19 @@ module Hscaffold
     , Permissions(..)
 
     -- * Types
+    , ScaffoldMonadT
+    , ScaffoldMonadIO
     , ScaffoldActionType(..)
     , ScaffoldAction
     , ScaffoldActionV
+    , ScaffoldMonadET
+
+    -- * Helpers
+    , runHscaffoldIO
+    , mkActionPath
 
     -- * Re-exports
-    , module Data.Text
+    , Text
     , module Control.Monad.IO.Class
     , module Control.Monad.Writer
     , module System.Directory
@@ -44,16 +64,17 @@ module Hscaffold
   where
 
 import           Control.Applicative
+import           Control.Arrow
 import           Control.Monad.IO.Class
 import           Control.Monad.Writer
 import           Data.Text              (Text)
-import qualified Data.Text
+import qualified Data.Text              as Text
 import qualified Data.Text.IO           as Text
 import           System.Directory
 -- TODO - Disable this on Windows
 import           System.FilePath
 import           System.Posix.Files
-
+import           Text.Regex
 
 -- | Run the scaffolding writer on the IO monad with no extensions
 --
@@ -67,29 +88,150 @@ import           System.Posix.Files
 --         file "./Main.hs" "main = putStrLn \\"Hello World\\""
 --         file "./Other.hs" "other = putStrLn \\"Hello You\\""
 -- @
-runHscaffold :: FilePath -> WriterT ScaffoldActionV IO a -> IO a
+runHscaffold :: MonadIO m => FilePath -> WriterT ScaffoldActionV m a -> m a
 runHscaffold root w = do
     (o, ws) <- runWriterT w
-    mapM_ (runAction root) ws
+    liftIO $ mapM_ (runAction root) ws
     return o
 
--- | Run a single scaffolding action on the IO monad with no extensions
-runAction :: FilePath -> ScaffoldActionType () -> IO ()
-runAction root (SetPermissions perms fp) =
-    setPermissions fp perms
-runAction root (Link fp1 fp2) =
-    createSymbolicLink fp1 fp2
-runAction root (File fp txt) =
-    Text.writeFile (root </> fp) txt
-runAction root (Directory fp nested) = do
-    createDirectoryIfMissing True (root </> fp)
-    mapM_ (runAction (root </> fp)) nested
-runAction root (Copy fp1 fp2) = do
-    let fp1' = makeAbsolute fp1
-        fp2' = makeAbsolute fp2
-    copyFile fp1' fp2'
+runHscaffoldIO :: FilePath -> ScaffoldMonadIO a -> IO a
+runHscaffoldIO = runHscaffold
+
+-- | Run the scaffolding writer and return an @.hsfiles@ 'Text' to use with
+-- @stack-templates@
+toHsfiles :: Writer ScaffoldActionV a -> Text
+toHsfiles w =
+    let (_, ws) = runWriter w
+    in Text.stripEnd $ Text.unlines $
+       concatMap (actionToHsfile ".") ws
+
+-- | Converts hsfiles to hscaffold actions
+fromHsfiles :: String -> ScaffoldAction e
+fromHsfiles h =
+    let (_, r) = foldr helper ("", []) matches
+    in r
   where
-    makeAbsolute fp = if isAbsolute fp then fp else root </> fp
+    helper (Just (fp:_), _) (lacc, current) =
+        ( ""
+        , File fp lacc:current
+        )
+    helper (_, l) (lacc, current) =
+        ( Text.stripEnd (l <> "\n" <> lacc)
+        , current
+        )
+    re = mkRegex "^{-# START_FILE (.+) #-}( +)?$"
+    matches = map (matchRegex re &&& Text.pack) (lines h)
+
+-- | Converts hsfiles to a hscaffold monad
+fromHsfilesW :: MonadWriter (ScaffoldAction e) m => String -> m ()
+fromHsfilesW h = tell (fromHsfiles h)
+
+-- | Converts a directory to scaffold actions
+hscaffoldFromDirectory :: FilePath -> IO (ScaffoldAction e)
+hscaffoldFromDirectory root = do
+    ls <- (filter (/= "..") . filter (/= ".")) <$> getDirectoryContents root
+    concat <$> mapM classify ls
+  where
+    classify fp = do
+        let fp' = root </> fp
+        isfl <- doesFileExist fp'
+        if isfl
+            then do
+                txt <- Text.readFile fp'
+                return [File fp txt]
+            else do
+                isdir <- doesDirectoryExist fp'
+                if isdir
+                   then hscaffoldFromDirectory fp'
+                   else return []
+
+hscaffoldToHaskell :: Foldable t => t (ScaffoldActionType e) -> Text
+hscaffoldToHaskell hs = Text.stripEnd $
+    Text.unlines $
+        concatMap hscaffoldActionToHaskell hs
+
+hscaffoldActionToHaskell :: ScaffoldActionType e -> [Text]
+hscaffoldActionToHaskell h = case h of
+    (File fp txt) ->
+        [ "file " <> Text.pack (show fp <> " " <> show txt) ]
+    (Directory fp nested) ->
+        let nestedCode = concatMap hscaffoldActionToHaskell nested
+            inestedCode = map ("    " <>) nestedCode
+        in [ "directory " <> Text.pack (show fp) <> " $ do"
+           ] <> inestedCode
+    (Link fp1 fp2) ->
+        [ "link " <> Text.pack (show fp1) <> " "
+          <> Text.pack (show fp2)
+        ]
+    (SetPermissions p fp) ->
+        [ "permissions " <> Text.pack (show fp)
+            <> " (" <> Text.pack (show p) <> ")"
+        ]
+    (Copy fp1 fp2) ->
+        [ "copy " <> Text.pack (show fp1)
+            <> " " <> Text.pack (show fp2)
+        ]
+    _ -> []
+
+-- | Shortcut for
+--
+-- @
+-- writeToHsfiles = do h <- toHsfiles w; liftIO $ Text.writeFile fp h
+-- @
+writeToHsfiles :: MonadIO m => FilePath -> Writer ScaffoldActionV a -> m ()
+writeToHsfiles fp w = do
+    let h = toHsfiles w
+    liftIO $ Text.writeFile fp h
+
+readHsfiles :: FilePath -> IO (ScaffoldAction e)
+readHsfiles fp = fromHsfiles <$> readFile fp
+
+-- | Convert a single scaffolding action to hsfiles
+--
+-- Ignores everything but the 'file' directives
+actionToHsfile :: FilePath -> ScaffoldActionType e -> [Text]
+actionToHsfile root a = case a of
+    (File fp txt) ->
+        [ "{-# START_FILE " <> Text.pack (mkActionPath root fp) <> " #-}" ] <>
+        Text.lines txt
+    (Directory fp nested) -> concatMap (actionToHsfile (mkActionPath root fp)) nested
+    _ -> []
+
+-- | Run a single scaffolding action on the IO monad with no extensions
+runAction :: FilePath -> ScaffoldActionType e -> IO ()
+runAction _ (ScaffoldActionTypeExtension _) = return ()
+runAction root (SetPermissions perms fp) =
+    setPermissions (mkActionPath root fp) perms
+runAction root (Link fp1 fp2) =
+    createSymbolicLink (mkActionPath root fp1) (mkActionPath root fp2)
+runAction root (File fp txt) =
+    Text.writeFile (mkActionPath root fp) txt
+runAction root (Directory fp nested) = do
+    createDirectoryIfMissing True (mkActionPath root fp)
+    mapM_ (runAction (mkActionPath root fp)) nested
+runAction root (Copy fp1 fp2) = do
+    let fp1' = mkActionPath root fp1
+        fp2' = mkActionPath root fp2
+    copyFile fp1' fp2'
+
+-- | Very simple helper, if the second argument is absolute, returns it,
+-- otherwise, return it relative to the first argument
+mkActionPath
+    :: FilePath
+    -- ^ The root
+    -> FilePath
+    -- ^ A filepath
+    -> FilePath
+mkActionPath root fp = if isAbsolute fp then fp else root </> fp
+
+-- | The writer monad transformer for scaffold actions
+type ScaffoldMonadT m a = WriterT ScaffoldActionV m a
+
+-- | The writer monad for scaffold actions, running in IO
+type ScaffoldMonadIO a = WriterT ScaffoldActionV IO a
+
+-- | The writer monad transformer for scaffold actions with an extension
+type ScaffoldMonadET e m a = WriterT (ScaffoldAction e) m a
 
 -- | Accumulator for actions
 type ScaffoldAction e = [ScaffoldActionType e]
--- test/HscaffoldSpec.hs
+++ test/HscaffoldSpec.hs
@@ -4,50 +4,153 @@ module HscaffoldSpec
 
 import           Control.Exception
 import           Control.Monad
+import           Data.Maybe
+import qualified Data.Text         as Text
 import           Hscaffold
-import           System.Directory
+import           System.IO.Temp
 
 import           Test.Hspec
 
+directoryShouldExist dir = do
+    e <- doesDirectoryExist dir
+    e `shouldBe` True
+
+fileShouldExist fp = do
+    e <- doesFileExist fp
+    e `shouldBe` True
+
+fileShouldHaveContents fp txt = do
+    e <- readFile fp
+    e `shouldBe` txt
+
+spec :: Spec
 spec = do
-    describe "the writers" $ do
+    describe "the writers" $
         it "create proper instructions for our results" $ do
             let (_, ws) = runWriter $ do
-                          file "stuff-bang.hs" ""
-                          directory "./something" $ do
-                              file "stuff-here.hs" ""
-                              file "stuff-there.hs" ""
+                    file "stuff-bang.hs" ""
+                    directory "./something" $ do
+                        file "stuff-here.hs" ""
+                        file "stuff-there.hs" ""
             (ws :: ScaffoldAction ())
                 `shouldBe` [ File "stuff-bang.hs" ""
-                           , Directory "./something" [ File "stuff-here.hs" ""
-                                                     , File "stuff-there.hs" ""
-                                                     ]
+                           , Directory "./something"
+                                       [ File "stuff-here.hs" ""
+                                       , File "stuff-there.hs" ""
+                                       ]
                            ]
 
-    describe "the runner" $ do
+    describe "runHscaffold" $
+        it "works without surprises" $ do
+            withSystemTempDirectory "hscaffold" $
+                \tmp -> do
+                    runHscaffold tmp $ do
+                        file "stuff-bang.hs" ""
+                        directory "./something" $ do
+                            file "stuff-here.hs" ""
+                            file "stuff-there.hs" ""
+                    directoryShouldExist (tmp </> "something")
+                    fileShouldExist (tmp </> "stuff-bang.hs")
+                    fileShouldExist (tmp </> "something" </> "stuff-here.hs")
+                    fileShouldExist (tmp </> "something" </> "stuff-there.hs")
+
+    describe "toHsfiles" $ do
+        it "works without surprises" $
+            let h = toHsfiles $ do
+                    file "stuff-bang.hs" "stuff here"
+                    file "other/stuff-bang.hs" "stuff here"
+                    directory "other" $ do
+                        file "other-stuff-bang.hs" "even more stuff here"
+            in h `shouldBe` Text.stripEnd
+               (Text.unlines [ "{-# START_FILE ./stuff-bang.hs #-}"
+                             , "stuff here"
+                             , "{-# START_FILE ./other/stuff-bang.hs #-}"
+                             , "stuff here"
+                             , "{-# START_FILE ./other/other-stuff-bang.hs #-}"
+                             , "even more stuff here"
+                             ])
+
+    describe "fromHsfiles" $ do
+        it "works without surprises" $
+            let h = (init
+                     (unlines
+                         [ "{-# START_FILE ./stuff-bang.hs #-}"
+                         , "stuff here"
+                         , "{-# START_FILE ./other/stuff-bang.hs #-}"
+                         , "stuff here"
+                         , "{-# START_FILE ./other/other-stuff-bang.hs #-}"
+                         , "even more stuff here"
+                         ]))
+            in (fromHsfiles h :: ScaffoldActionV) `shouldBe`
+               [ File "./stuff-bang.hs" "stuff here"
+               , File "./other/stuff-bang.hs" "stuff here"
+               , File "./other/other-stuff-bang.hs" "even more stuff here"
+               ]
+
+        it "works without surprises" $
+            let h = (init
+                     (unlines
+                         [ "{-# START_FILE ./stuff-bang.hs #-}"
+                         , "stuff here"
+                         , "multiline"
+                         , " ya"
+                         , "{-# START_FILE ./other/stuff-bang.hs #-}"
+                         , "stuff here"
+                         , ""
+                         , "{-# START_FILE ./other/other-stuff-bang.hs #-}      "
+                         , "even more stuff here"
+                         , "  here too"
+                         ]))
+            in (fromHsfiles h :: ScaffoldActionV) `shouldBe`
+               [ File "./stuff-bang.hs" "stuff here\nmultiline\n ya"
+               , File "./other/stuff-bang.hs" "stuff here"
+               , File "./other/other-stuff-bang.hs" "even more stuff here\n  here too"
+               ]
+
+    describe "hscaffoldToHaskell" $ do
+        it "works without surprises" $
+            let h = hscaffoldToHaskell $ execWriter $ do
+                    file "stuff-bang.hs" "stuff here"
+                    file "other/stuff-bang.hs" "stuff here"
+                    directory "other" $ do
+                        file "other-stuff-bang.hs" "even more stuff here"
+            in h `shouldBe` Text.stripEnd
+               (Text.unlines [ "file \"stuff-bang.hs\" \"stuff here\""
+                             , "file \"other/stuff-bang.hs\" \"stuff here\""
+                             , "directory \"other\" $ do"
+                             , "    file \"other-stuff-bang.hs\" \"even more stuff here\""
+                             ])
+
+    describe "hscaffoldFromDirectory" $ do
+        it "works without surprises" $ do
+            h <- hscaffoldFromDirectory "./src" :: IO (ScaffoldActionV)
+            let ms = mapMaybe (\x -> case x of File fp _ -> Just fp; _ -> Nothing) h
+            ms `shouldContain` ["Hscaffold.hs"]
+
+    describe "the runner" $
         describe "runAction" $ do
             it "creates files" $ do
-                void $ (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
-                void $ (try $ removeFile "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeFile "./tmp" :: IO (Either SomeException ()))
                 runAction "." (File "./tmp" "stuff")
                 ecnts <- try $ readFile "./tmp" :: IO (Either SomeException String)
-                void $ (try $ removeFile "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeFile "./tmp" :: IO (Either SomeException ()))
                 let Right cnts = ecnts
                 cnts `shouldBe` "stuff"
 
             it "creates directories" $ do
-                void $ (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
-                void $ (try $ removeFile "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeFile "./tmp" :: IO (Either SomeException ()))
                 runAction "." (Directory "./tmp/" [])
                 t <- doesDirectoryExist "./tmp"
-                void $ (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
                 t `shouldBe` True
 
             it "creates directory contents" $ do
-                void $ (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
-                void $ (try $ removeFile "./tmp" :: IO (Either SomeException ()))
-                runAction "." (Directory "./tmp/" [File "stuff" "here"])
+                void (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeFile "./tmp" :: IO (Either SomeException ()))
+                runAction "." (Directory "./tmp/" [ File "stuff" "here" ])
                 ecnts <- try $ readFile "./tmp/stuff" :: IO (Either SomeException String)
-                void $ (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
+                void (try $ removeDirectoryRecursive "./tmp" :: IO (Either SomeException ()))
                 let Right cnts = ecnts
                 cnts `shouldBe` "here"
